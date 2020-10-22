bb5298f1e26169a1e0a5b8e91b22d4c005d25867
abramovvicz <abramovvicz@gmail.com>
2020-10-22 20:29:11 +0200

    some add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,55 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h4>HELLO WOLRD</h4>
+    <h2>{{ name }}</h2>
+    <div class="row" v-for="t in tasks" v-bind:key="t.action">
+      <div class="col">{{t.action}}</div>
+      <div class="col">{{t.done}}</div>
+    </div>
+    <div>
+      <span
+        v-for="item in map"
+        :key="item.id"
+      ></span>
+    </div>
+    <button class="btn" v-on:click="getMeasruement"> SOME BUTTON </button>
   </div>
 </template>
 
 <script>
+const url = "http://localhost:9092/measurements/";
 export default {
-  name: 'HelloWorld',
+  data() {
+    return {
+      name: "DUPA",
+      tasks: [
+        { action: "Zrób cos chuju", done: false },
+        { action: "Nie rób cos chuju", done: false },
+      ],
+      flag:false,
+      measurements:[],
+      map:new Map(),
+      item:""
+    };
+  },
+  computed:{
+    chagneFlag(){
+      return this.flag?this.tasks.filter(t=> !t.done):this.tasks
+    }
+  },
+  methods:{
+      getMeasruement(){
+        this.map = new Map()
+        this.$http.get(url).then((response)=>{
+          response.data.result.forEach(item => this.map.set(item.id, item.name));
+        })
+      }
+  },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
