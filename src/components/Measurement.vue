405c86dbd1eba3deb71a01c167d96d3e9b037615
abramovvicz <abramovvicz@gmail.com>
2020-11-03 23:11:46 +0100

    update measurement form

--- src/components/Measurement.vue
+++ src/components/Measurement.vue
@@ -1,18 +1,21 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col justify="left" align="left"
-        >Data pomiaru: {{ currentDateWithNeededFormat | formatDate }}
-      </v-col>
-      <v-col justify="left" align="left">Termin następnego pomiaru: </v-col>
-    </v-row>
-    <v-row>
-      <v-col
-        ><v-form ref="form" v-model="valid" lazy-validation>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-row>
+        <v-col justify="left" align="left"
+          >Data pomiaru: {{ form.currentDateWithNeededFormat | formatDate }}
+        </v-col>
+        <v-col justify="left" align="left"
+          >Termin następnego pomiaru:
+          {{ form.nextMeasurementDate | formatDate }}</v-col
+        >
+      </v-row>
+      <v-row>
+        <v-col>
           <v-row>
             <v-col>
               <v-text-field
-                v-model="name"
+                v-model="form.nameNet"
                 :counter="10"
                 :rules="nameRules"
                 label="Nazwa sieci"
@@ -21,7 +24,7 @@
             </v-col>
             <v-col>
               <v-text-field
-                v-model="name"
+                v-model="form.address"
                 :counter="10"
                 :rules="nameRules"
                 label="Adres pomiaru"
@@ -33,38 +36,62 @@
           <v-row>
             <v-col justify="left" align="left">
               Wybierz rodzaj hydrantu:
-              <v-radio-group v-model="row" row>
-                <v-radio label="Zewnętrzny" value="radio-1"></v-radio>
-                <v-radio label="Wewnętrzny" value="radio-2"></v-radio>
+              <v-radio-group
+                v-model="form.hideHydrantInOutType"
+                :mandatory="false"
+                row
+              >
+                <v-radio label="Zewnętrzny" :value="1"></v-radio>
+                <v-radio label="Wewnętrzny" :value="0"></v-radio>
               </v-radio-group>
             </v-col>
           </v-row>
-          <v-row>
+          <v-row v-if="form.hideHydrantInOutType === 1">
             <v-col justify="left" align="left">
-              Jeśli Zewnętrzny to wybierz podz czy naziemny (dodać funk ze samo
-              sie pjawia luz znika)
-              <v-radio-group v-model="row" row>
-                <v-radio label="Nadziemny" value="radio-1"></v-radio>
-                <v-radio label="Podziemny" value="radio-2"></v-radio>
+              Wybierz typ hydrantu:
+              <v-radio-group
+                v-model="form.hideHydrantDiameters"
+                :mandatory="false"
+                row
+              >
+                <v-radio label="Nadziemny" :value="1"></v-radio>
+                <v-radio label="Podziemny" :value="0"></v-radio>
               </v-radio-group>
             </v-col>
           </v-row>
-          <v-row>
+          <v-row v-if="form.hideHydrantInOutType === 1">
             <v-col justify="left" align="left">
               Wybierz średnicę:
-              <v-radio-group v-model="row" row>
+              <v-radio-group v-model="form.diameter" row>
                 <v-radio label="DN80" value="radio-1"></v-radio>
                 <v-radio label="DN100" value="radio-2"></v-radio>
-                <v-radio label="DN150" value="radio-3"></v-radio>
-                <v-radio label="DN200" value="radio-4"></v-radio>
+                <v-radio
+                  v-if="form.hideHydrantDiameters === 1"
+                  label="DN150"
+                  value="radio-3"
+                ></v-radio>
+                <v-radio
+                  v-if="form.hideHydrantDiameters === 1"
+                  label="DN200"
+                  value="radio-4"
+                ></v-radio>
               </v-radio-group>
             </v-col>
           </v-row>
-
+          <v-row v-if="form.hideHydrantInOutType === 0">
+            <v-col justify="left" align="left">
+              Wybierz średnicę:
+              <v-radio-group v-model="form.diameter" row>
+                <v-radio label="DN25" value="radio-1"></v-radio>
+                <v-radio label="DN33" value="radio-2"></v-radio>
+                <v-radio label="DN52" value="radio-3"></v-radio>
+              </v-radio-group>
+            </v-col>
+          </v-row>
           <v-row>
             <v-col justify="left" align="left">
               Dostępość do hydrantu:
-              <v-radio-group v-model="row" row>
+              <v-radio-group v-model="form.hydrantAvailable" row>
                 <v-radio label="TAK" value="radio-1"></v-radio>
                 <v-radio label="NIE" value="radio-2"></v-radio>
               </v-radio-group>
@@ -74,7 +101,7 @@
           <v-row>
             <v-col justify="left" align="left">
               Dostępość do zasuwy:
-              <v-radio-group v-model="row" row>
+              <v-radio-group v-model="form.barAvailable" row>
                 <v-radio label="TAK" value="radio-1"></v-radio>
                 <v-radio label="NIE" value="radio-2"></v-radio>
               </v-radio-group>
@@ -84,7 +111,7 @@
           <v-row>
             <v-col justify="left" align="left">
               Oznakowanie hydrantu znakiem 'H'
-              <v-radio-group v-model="row" row>
+              <v-radio-group v-model="form.asignH" row>
                 <v-radio label="TAK" value="radio-1"></v-radio>
                 <v-radio label="NIE" value="radio-2"></v-radio>
               </v-radio-group>
@@ -94,7 +121,7 @@
           <v-row>
             <v-col justify="left" align="left">
               Oznakowanie hydrantu tablicznką o.u.p.w.
-              <v-radio-group v-model="row" row>
+              <v-radio-group v-model="form.asignOUPW" row>
                 <v-radio label="TAK" value="radio-1"></v-radio>
                 <v-radio label="NIE" value="radio-2"></v-radio>
               </v-radio-group>
@@ -104,7 +131,7 @@
           <v-row>
             <v-col justify="left" align="left">
               Oznakowanie zasuwy tablicznką o.u.p.w.
-              <v-radio-group v-model="row" row>
+              <v-radio-group v-model="form.asignBarOUPW" row>
                 <v-radio label="TAK" value="radio-1"></v-radio>
                 <v-radio label="NIE" value="radio-2"></v-radio>
               </v-radio-group>
@@ -114,7 +141,7 @@
           <v-row>
             <v-col justify="left" align="left">
               Stan nasad Storz`a
-              <v-radio-group v-model="row" row>
+              <v-radio-group v-model="form.storz" row>
                 <v-radio label="OK" value="radio-1"></v-radio>
                 <v-radio label="NIE OK" value="radio-2"></v-radio>
               </v-radio-group>
@@ -124,7 +151,7 @@
           <v-row>
             <v-col justify="left" align="left">
               Stan uszczelek
-              <v-radio-group v-model="row" row>
+              <v-radio-group v-model="form.rubber" row>
                 <v-radio label="OK" value="radio-1"></v-radio>
                 <v-radio label="NIE OK" value="radio-2"></v-radio>
               </v-radio-group>
@@ -134,7 +161,7 @@
           <v-row>
             <v-col justify="left" align="left">
               Ilość pokryw
-              <v-radio-group v-model="row" row>
+              <v-radio-group v-model="form.cover" row>
                 <v-radio label="1" value="radio-1"></v-radio>
                 <v-radio label="2" value="radio-2"></v-radio>
                 <v-radio label="3" value="radio-3"></v-radio>
@@ -145,62 +172,66 @@
           <v-row>
             <v-col justify="left" align="left">
               Odległość od najbliższego budynku
-              <v-radio-group v-model="row" row>
+              <v-radio-group v-model="form.nearestBuilding" row>
                 <v-radio label="OK" value="radio-1"></v-radio>
                 <v-radio label="NIE OK" value="radio-2"></v-radio>
               </v-radio-group>
             </v-col>
           </v-row>
-
-          <v-text-field
-            v-model="name"
-            :counter="10"
-            :rules="nameRules"
-            label="Ciśnienie statyczne"
-            required
-          ></v-text-field>
-
-          <v-text-field
-            v-model="name"
-            :counter="10"
-            :rules="nameRules"
-            label="Ciśnienie dynamiczne"
-            required
-          ></v-text-field>
-
-          <v-text-field
-            v-model="name"
-            :counter="10"
-            :rules="nameRules"
-            label="Wydajność hydrantu"
-            required
-          ></v-text-field>
 
           <v-row>
             <v-col justify="left" align="left">
-              Odległość od najbliższego budynku
-              <v-radio-group v-model="row" row>
-                <v-radio label="OK" value="radio-1"></v-radio>
-                <v-radio label="NIE OK" value="radio-2"></v-radio>
+              Odwodnienie hydrantu:
+              <v-radio-group v-model="form.dehydration" row>
+                <v-radio label="NIE ODWADNIA SIĘ" value="radio-1"></v-radio>
+                <v-radio label="ODWADNIA SIĘ" value="radio-2"></v-radio>
               </v-radio-group>
             </v-col>
           </v-row>
 
-          <!-- <v-btn
-            :disabled="!valid"
+          <v-text-field
+            v-model="form.staticPreasure"
+            :rules="nameRules"
+            label="Ciśnienie statyczne"
+            suffix="Mpa"
+            type="number"
+            maxlength="10"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="form.dynamicPreasure"
+            :rules="nameRules"
+            label="Ciśnienie dynamiczne"
+            suffix="Mpa"
+            type="number"
+            maxlength="10"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="form.hydrantPerformence"
+            :rules="nameRules"
+            label="Wydajność hydrantu"
+            suffix="Mpa"
+            type="number"
+            required
+          ></v-text-field>
+
+          <v-btn
             color="success"
             class="mr-4"
-            @click="validate"
+            @click="sendFormMeasurementToBackendSecond"
           >
             ZAPISZ
           </v-btn>
-
+          <!--
           <v-btn color="error" class="mr-4" @click="reset"> WYCZYŚĆ </v-btn>
 
           <v-btn color="warning" @click="resetValidation"> ANULUJ </v-btn> -->
-        </v-form></v-col
-      >
-    </v-row>
+        </v-col>
+      </v-row>
+    </v-form>
   </v-container>
 </template>
 
@@ -273,13 +304,63 @@
 
 
 <script>
+import moment from "moment";
+import axios from "axios";
 export default {
   mounted: function () {
     this.checkCurrentDate();
+    this.countNextMeasurementDate();
   },
   data() {
-    return {
+    const measurementForm = Object.freeze({
       currentDateWithNeededFormat: "",
+      nextMeasurementDate: "",
+      hideHydrantInOutType: 0,
+      hideHydrantDiameters: 0,
+      reset: true,
+      diameter: "",
+      hydrantAvailable: "",
+      barAvailable: "",
+      nameNet: "",
+      address: "",
+      asignH: "",
+      asignOUPW: "",
+      asignBarOUPW: "",
+      storz: "",
+      rubber: "",
+      cover: "",
+      nearestBuilding: "",
+      staticPreasure: "",
+      dynamicPreasure: "",
+      hydrantPerformence: "",
+      dehydration: "",
+
+      //zamienic napelnoprawny JSON 
+
+//       {
+//   "address": "string",
+//   "createdAt": "2020-11-03T22:09:04.583Z",
+//   "descriptions": [
+//     {
+//       "comments": "string",
+//       "createdAt": "2020-11-03T22:09:04.583Z",
+//       "id": 0,
+//       "measurementId": 0,
+//       "name": "string",
+//       "status": 0,
+//       "updatedAt": "2020-11-03T22:09:04.583Z"
+//     }
+//   ],
+//   "hydrantDiameter": "DN100",
+//   "hydrantSubType": "GROUND",
+//   "hydrantType": "INSIDE",
+//   "id": 0,
+//   "photo": "string",
+//   "updatedAt": "2020-11-03T22:09:04.583Z"
+// }
+    });
+    return {
+      form: Object.assign({}, measurementForm),
       currentDate: "",
       valid: true,
       name: "",
@@ -295,8 +376,10 @@ export default {
       select: null,
       items: ["Item 1", "Item 2", "Item 3", "Item 4"],
       checkbox: false,
-
-      reset: true,
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      ipAddress: "http://localhost:9092",
     };
   },
   methods: {
@@ -311,11 +394,55 @@ export default {
     // },
     checkCurrentDate() {
       this.currentDate = new Date();
-      this.currentDateWithNeededFormat = this.currentDate
+      this.form.currentDateWithNeededFormat = this.currentDate
         .toJSON()
         .slice(0, 10)
         .replace(/-/g, ".");
-      console.log(this.currentDateWithNeededFormat + "Data");
+    },
+    countNextMeasurementDate() {
+      this.form.nextMeasurementDate = moment(this.currentDateWithNeededFormat);
+      this.form.nextMeasurementDate.add(1, "y");
+    },
+    sendFormMeasurementToBackend() {
+      Object.keys(this.form).forEach((value) => {
+        console.log(value);
+      });
+      this.$http
+        .post(
+          this.ipAddress + "/measurements",
+          JSON.stringify(this.form),
+          this.headers
+        )
+        .then(
+          (data) => {
+            // this.sendResult = 1;
+            console.log(data);
+            // this.result = "Successfully add user to the database";
+            // this.checkIdFromNewAddedUser();
+          },
+          (data) => {
+            if (data.status === 403) {
+              console.log("Error: " + data.status + data.message);
+            } else if (data.status === 500) {
+              this.sendResult = 2;
+              this.result = "ERROR - some error. Please be patient";
+            }
+          }
+        );
+    },
+
+    sendFormMeasurementToBackendSecond() {
+      return axios
+        .post("http://localhost:9092/measurements/", {
+         form:'form'
+        })
+        .then((response) => {
+          JSON.stringify(this.form), this.headers;
+          console.log(response.data.result);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     },
   },
 };
